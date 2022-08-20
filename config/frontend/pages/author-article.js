ef1dd5875719ee16b2c78ddcd88702b8c13d10e1
Richard Lovell <rjlovell@hotmail.com>
2022-08-20 23:11:28 +1200

    First commit

---
+++ config/frontend/pages/author-article.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { getProps } from "@builtjs/site";
+import Error from "next/error";
+import { withRouter } from "next/router";
+import ProfileArticle1 from "@/templates/articles/profile-article-1/profile-article-1";
+import Layout from "@/layout/layout";
+
+const Page = (props) => {
+  if (props.error) {
+    return <Error statusCode={props.error.code} />
+  }
+  return (
+    <><Layout headerContent={props.headerContent}  footerContent={props.footerContent} >
+			 <ProfileArticle1 content={props.authorArticleContent} />
+			</Layout>
+		</>
+  );
+};
+
+export default withRouter(Page);
+
+export async function getStaticProps() {
+  let props = await getProps({pageSlug: 'author-article'});
+  return {
+    props: props,
+    revalidate: 10
+  };
+}
